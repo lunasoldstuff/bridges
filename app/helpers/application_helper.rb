0958febdfc61447a56242e5bd7e34cfc08b5c0c2
Yamagishi Kazutoshi <ykzts@desire.sh>
2018-06-18 20:01:24 +0900

    Disable Sprockets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,10 @@ module ApplicationHelper
   def mastodon?
     user_signed_in? && !current_user.mastodon.nil?
   end
+
+  def fa_icon(name, **options)
+    classes  = (options.delete(:class) || '').split(/\s+/)
+    classes += ['fa', "fa-#{name}"]
+    content_tag(:i, nil, options.merge(class: classes.join(' ')))
+  end
 end
---
+++ app/javascript/images/index.js
@@ -0,0 +1,5 @@
+import './logo_full.svg';
+import './logo.svg';
+import './preview.jpg';
+import './toot-friend.png';
+import './twitter-square.png';
